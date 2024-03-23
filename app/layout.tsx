c2ab168ad802d498f6423cef5bcf484f051413ce
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-03-23 12:27:07 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const sans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  style: "normal",
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex-sans",
+});
+
+export const metadata: Metadata = {
+  title: "EAGER - Imagine",
+  description: "Imagine is a simple image sharing platform.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: "#5FBDFF"}// Set the primary color to gold
+    }}>
+      <html lang="en">
+        <body className={cn("font-sans antialised", sans.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
